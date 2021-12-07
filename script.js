7bc64769db07d3f2571e77bf22b65e92d80dff87
Charles <66703824+chardynamics@users.noreply.github.com>
2021-12-07 08:35:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var scene = 1;
 var fade = 130;
 var arialBold;
+var scaleResolutionX;
+var scaleResolutionY;
+
+//probably should find a better solution
+scaleResolutionX = windowWidth/1517;
+scaleResolutionY = windowHeight/853;
+
 
 function preload() {
 	arialBold = loadFont("/assets/fonts/arialbd.ttf");
@@ -19,7 +26,7 @@ function intro() {
 	background(0, 0, 0);
 	fill(255, 255, 255);
 	textSize(750);
-	text("D P", windowWidth/4, windowHeight/2);
+	text("DP", 500 * scaleResolutionX, windowHeight/2;
 }
 
 function menu() {
