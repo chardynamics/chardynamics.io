4c9b9a8b107a2fa16f886c6b45167da436bafc83
Charles <66703824+chardynamics@users.noreply.github.com>
2022-02-15 13:21:26 -0500

    Initial commit

---
+++ projects/p5svg/script.js
@@ -0,0 +1,28 @@
+function setup() {
+	var canvas = createCanvas(windowWidth, windowHeight, SVG);
+	canvas.style('margin', 'auto');
+	canvas.parent('script-holder');
+	
+	rectMode(CENTER);
+	angleMode(DEGREES);
+	noStroke();
+	
+	var privacyBanner = document.querySelectorAll("[data-gg-privacy-banner-anchor]");
+	for (var i = 0; i < privacyBanner.length; i++) {
+		privacyBanner[i].parentNode.removeChild(privacyBanner[i]);
+	}
+}
+
+function draw() {
+	clear();
+	rect(0, 0, 100, 100);
+	if (frameCount > 100) {
+        noLoop();
+        save();
+    }
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
+}
+ 
