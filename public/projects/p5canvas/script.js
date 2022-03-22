25bfdf39344d68a9e0d67a0ee84b6f37609f72f0
chardynamics <hoi4devalt@gmail.com>
2022-03-22 18:08:54 +0000

    Initial commit

---
+++ public/projects/p5canvas/script.js
@@ -0,0 +1,44 @@
+let canvasHeight = 50;
+let canvasWidth = 60;
+let code;
+let inputValue;
+
+function setup() {
+	var canvas = createCanvas(canvasHeight, canvasWidth);
+	canvas.style('margin', 'auto');
+	canvas.parent('script-holder');
+	
+	rectMode(CENTER);
+	angleMode(DEGREES);
+	noStroke();
+	
+	var privacyBanner = document.querySelectorAll("[data-gg-privacy-banner-anchor]");
+	for (var i = 0; i < privacyBanner.length; i++) {
+		privacyBanner[i].parentNode.removeChild(privacyBanner[i]);
+	}
+}
+
+function draw() {
+	clear();
+	eval(code);
+}
+
+function update() {
+	inputValue = document.getElementById("input").value;
+	code = inputValue;
+}
+
+function changeHeight(newHeight) {
+	canvasHeight = newHeight;
+	resizeCanvas(canvasHeight, canvasWidth);
+}
+
+function changeWidth(newWidth) {
+	canvasWidth = newWidth;
+	resizeCanvas(canvasHeight, canvasWidth);
+}
+
+function windowResized() {
+	resizeCanvas(canvasHeight, canvasWidth);
+}
+ 
